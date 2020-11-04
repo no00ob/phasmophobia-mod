205aa6e66a4604f82ff8ef795d3e2c7b042d3518
no00ob <aksu.kotikone@gmail.com>
2020-11-04 02:33:30 +0200

    Added all initial files.

---
+++ Phasmophobia Mod/ModMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace Phasmophobia_Mod
+{
+    public class ModMain : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("Phasmophobia 8 player mod loaded!");
+        }
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                MelonLogger.Log("Debug Test. You pressed the 'P' key.");
+            }
+        }
+    }
+}
---
+++ Phasmophobia Mod/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using Phasmophobia_Mod;
+// ...
+[assembly: MelonInfo(typeof(ModMain), "8 Player Mod", "1.0.0", "no00ob")]
+[assembly: MelonGame("Kinetic Games", "Phasmophobia")]
